2eb6ab072a05feb7a5b3512f607bf2a82034b4b3
Partha Guha Roy <partha@cefalo.com>
2017-11-03 16:40:55 +0600

    More inline with what I want. The real fix is to pass these values as props. But I don't have that time now :-(

--- index.js
+++ index.js
@@ -150,14 +150,14 @@ const styles = StyleSheet.create({
   text_msg: {
     fontSize: 14,
     flex: 1,
-    paddingLeft: 24,    
+    paddingLeft: 20,    
     paddingTop: 14,
     paddingBottom: 14
   },
   action_text: {
     fontSize: 14,
     fontWeight: '600',
-    paddingRight: 24,
+    paddingRight: 20,
     paddingTop: 14,
     paddingBottom: 14
   }
