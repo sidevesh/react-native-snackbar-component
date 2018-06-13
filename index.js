102319c5176ffac6a69a586ce0283ab01c44b400
QifanViseo <qifan.yang.viseo@gmail.com>
2018-06-13 14:48:25 +0200

    correct snackbar

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ class SnackbarComponent extends Component {
           }}
         >
           <Text style={[styles.text_msg, {color: this.props.messageColor}]}>{this.props.textMessage}</Text>
-          {this.props.actionHandler && this.props.actionText &&
+          {this.props.actionHandler && !!this.props.actionText &&
             <Touchable onPress={() => {this.props.actionHandler()}} >
               <Text style={[styles.action_text, {color: this.props.accentColor}]}>{this.props.actionText.toUpperCase()}</Text>
             </Touchable>
