8f267ae0a0841b41762848ded814d3c6c156ffea
Matúš Hrúz <hruzmatus@gmail.com>
2020-01-06 19:58:49 +0100

    feat: refactor using hooks

--- index.js
+++ index.js
@@ -1,152 +1,142 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import {
-  StyleSheet, Text, Animated, Easing,
-} from 'react-native';
-import { Touchable } from './src';
-import { noop } from './src/utils';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, Text, Animated, Easing } from "react-native";
+import { Touchable } from "./src";
+import { noop } from "./src/utils";
 /*
  * Values are from https://material.io/guidelines/motion/duration-easing.html#duration-easing-dynamic-durations
  */
 
 const easingValues = {
   entry: Easing.bezier(0.0, 0.0, 0.2, 1),
-  exit: Easing.bezier(0.4, 0.0, 1, 1),
+  exit: Easing.bezier(0.4, 0.0, 1, 1)
 };
 
 const durationValues = {
   entry: 225,
-  exit: 195,
+  exit: 195
 };
 
-class SnackbarComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      translateValue: new Animated.Value(0),
-      hideDistance: 9999,
-    };
-  }
+const SnackbarComponent = ({
+  distanceCallback,
+  backgroundColor,
+  actionHandler,
+  messageColor,
+  accentColor,
+  textMessage,
+  actionText,
+  position,
+  visible,
+  bottom,
+  left,
+  right
+}) => {
+  const [hideDistance, setHideDistance] = useState(9999);
+  const translateVal = useRef(new Animated.Value(0));
+  const translateValue = translateVal.current;
+  const positionMap = { light, left, bottom };
 
-  render() {
-    return (
-      <Animated.View
-        style={[
-          styles.limit_container,
-          {
-            height: this.state.translateValue.interpolate({
-              inputRange: [0, 1],
-              outputRange: [0, this.state.hideDistance],
-            }),
-            backgroundColor: this.props.backgroundColor,
-          },
-          { [this.props.position]: this.props[this.props.position] },
-        ]}
-      >
-        <Animated.View
-          style={[
-            styles.container,
-            {
-              backgroundColor: this.props.backgroundColor,
-              left: this.props.left,
-              right: this.props.right,
-            },
-            {
-              [this.props.position]: this.state.translateValue.interpolate({
-                inputRange: [0, 1],
-                outputRange: [this.state.hideDistance * -1, 0],
-              }),
-            },
-          ]}
-          onLayout={(event) => {
-            this.setState({ hideDistance: event.nativeEvent.layout.height });
-          }}
-        >
-          <Text style={[styles.text_msg, { color: this.props.messageColor }]}>
-            {this.props.textMessage}
-          </Text>
-          {this.props.actionHandler && !!this.props.actionText && (
-            <Touchable
-              onPress={() => {
-                this.props.actionHandler();
-              }}
-            >
-              <Text
-                style={[styles.action_text, { color: this.props.accentColor }]}
-              >
-                {this.props.actionText.toUpperCase()}
-              </Text>
-            </Touchable>
-          )}
-        </Animated.View>
-      </Animated.View>
-    );
-  }
+  const hideSnackbar = () => {
+    Animated.timing(translateValue, {
+      duration: durationValues.exit,
+      easing: easingValues.exit,
+      toValue: 0
+    }).start();
+  };
 
-  componentDidMount() {
-    if (this.props.visible) {
-      this.state.translateValue.setValue(1);
-    } else {
-      this.state.translateValue.setValue(0);
-    }
-  }
+  useEffect(() => {
+    translateValue.setValue(visible ? 1 : 0);
+  }, []);
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.visible && !this.props.visible) {
-      Animated.timing(this.state.translateValue, {
+  // componentWillReceiveProps
+  useEffect(() => {
+    if (visible && !prevVisible) {
+      Animated.timing(translateValue, {
         duration: durationValues.entry,
         toValue: 1,
-        easing: easingValues.entry,
+        easing: easingValues.entry
       }).start();
       if (nextProps.autoHidingTime) {
-        const hideFunc = this.hideSnackbar.bind(this);
-        setTimeout(hideFunc, nextProps.autoHidingTime);
+        setTimeout(hideSnackbar, nextProps.autoHidingTime);
       }
-    } else if (!nextProps.visible && this.props.visible) {
-      this.hideSnackbar();
+    } else if (!visible && prevVisible) {
+      hideSnackbar();
     }
-  }
+  }, [visible]);
 
-  componentWillUpdate(nextProps, nextState) {
-    if (
-      nextProps.visible !== this.props.visible
-      || nextState.hideDistance !== this.state.hideDistance
-    ) {
-      if (nextProps.visible) {
-        this.props.distanceCallback(nextState.hideDistance + this.props.bottom);
+  // componentWillUpdate
+  useEffect(() => {
+    if (visible !== prevVisible || hideDistance !== prevHideDistance) {
+      if (visible) {
+        distanceCallback && distanceCallback(nexthideDistance + bottom);
       } else {
-        this.props.distanceCallback(this.props.bottom);
+        distanceCallback && distanceCallback(bottom);
       }
     }
-  }
+  }, [visible, hideDistance]);
 
-  /**
-   * Starting te animation to hide the snack bar.
-   * @return {null} No return.
-   */
-  hideSnackbar() {
-    Animated.timing(this.state.translateValue, {
-      duration: durationValues.exit,
-      toValue: 0,
-      easing: easingValues.exit,
-    }).start();
-  }
-}
+  return (
+    <Animated.View
+      style={[
+        styles.limit_container,
+        {
+          height: translateValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, hideDistance]
+          }),
+          backgroundColor: backgroundColor
+        },
+        { [position]: positionMap[position] }
+      ]}
+    >
+      <Animated.View
+        style={[
+          styles.container,
+          { backgroundColor, left, right },
+          {
+            [position]: translateValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: [hideDistance * -1, 0]
+            })
+          }
+        ]}
+        onLayout={event => {
+          setHideDistance(event.nativeEvent.layout.height);
+        }}
+      >
+        <Text style={[styles.text_msg, { color: messageColor }]}>
+          {textMessage}
+        </Text>
+        {actionHandler && !!actionText && (
+          <Touchable
+            onPress={() => {
+              actionHandler();
+            }}
+          >
+            <Text style={[styles.action_text, { color: accentColor }]}>
+              {actionText.toUpperCase()}
+            </Text>
+          </Touchable>
+        )}
+      </Animated.View>
+    </Animated.View>
+  );
+};
 
 SnackbarComponent.defaultProps = {
-  accentColor: 'orange',
-  messageColor: '#FFFFFF',
-  backgroundColor: '#484848',
+  accentColor: "orange",
+  messageColor: "#FFFFFF",
+  backgroundColor: "#484848",
   distanceCallback: noop,
   actionHandler: noop,
   left: 0,
   right: 0,
   bottom: 0,
   visible: false,
-  position: 'bottom',
-  actionText: '',
-  textMessage: '',
-  autoHidingTime: 0, // Default value will not auto hide the snack bar as the old version.
+  position: "bottom", // left, right, bottom
+  actionText: "",
+  textMessage: "",
+  autoHidingTime: 0 // Default value will not auto hide the snack bar as the old version.
 };
 
 SnackbarComponent.propTypes = {
@@ -161,38 +151,38 @@ SnackbarComponent.propTypes = {
   visible: PropTypes.bool,
   actionText: PropTypes.string,
   textMessage: PropTypes.string,
-  position: PropTypes.oneOf(['bottom', 'top']), // bottom (default), top
-  autoHidingTime: PropTypes.number, // How long (in milliseconds) the snack bar will be hidden.
+  position: PropTypes.oneOf(["bottom", "top"]), // bottom (default), top
+  autoHidingTime: PropTypes.number // How long (in milliseconds) the snack bar will be hidden.
 };
 
 const styles = StyleSheet.create({
   limit_container: {
-    position: 'absolute',
-    overflow: 'hidden',
+    position: "absolute",
+    overflow: "hidden",
     left: 0,
     right: 0,
-    zIndex: 9999,
+    zIndex: 9999
   },
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    position: 'absolute',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    position: "absolute"
   },
   text_msg: {
     fontSize: 14,
     flex: 1,
     paddingLeft: 20,
     paddingTop: 14,
-    paddingBottom: 14,
+    paddingBottom: 14
   },
   action_text: {
     fontSize: 14,
-    fontWeight: '600',
+    fontWeight: "600",
     paddingRight: 20,
     paddingTop: 14,
-    paddingBottom: 14,
-  },
+    paddingBottom: 14
+  }
 });
 
 export default SnackbarComponent;
