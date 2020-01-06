a8a807ec7ab7f2f4ed1510abe86e3da736f1feec
Matúš Hrúz <hruzmatus@gmail.com>
2020-01-06 20:03:54 +0100

    fix: import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import { StyleSheet, Text, Animated, Easing } from "react-native";
 import { Touchable } from "./src";
